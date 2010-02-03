dbfa45bc9d5a21557135cb1de8128d3862f1da9c
Jorge Luis Sacchini <Jorge-Luis.Sacchini@inria.fr>
2010-02-03 14:24:14 +0000

    Better pretty printer

--- Abstract.hs
+++ Abstract.hs
@@ -73,20 +73,34 @@ tprint :: Int -> [Name] -> Expr -> Doc
 tprint p l (Ann _ t u) = parensIf (p > 1) $ tprint 2 l t <> text " :: " <> tprint 0 l u
 tprint p l (TSort _ s) = text $ show s
 tprint p l t@(Pi _ x t1 t2) | not (isFree 0 t2) = parensIf (p > 0) $ sep [tprint 1 l t1 <> text " ->", tprint 0 (x:l) t2]
-                            | otherwise = parensIf (p > 0) $ text "forall" <> nestedPi l t
+                            | otherwise = parensIf (p > 0) $ text "forall " <> nestedPi l t
 tprint p l (Bound _ n) = text $ showboundInt l n
-tprint p l t@(Lam _ x t1 t2) = parensIf (p > 0) $ text "fun" <> nestedLam l t
+tprint p l t@(Lam _ x t1 t2) = parensIf (p > 0) $ text "fun " <> nestedLam l t
 tprint p l (Free _ x) = text x
 tprint p l (App _ t1 t2) = parensIf (p > 2) $ sep [tprint 2 l t1, nest 2 $ tprint 3 l t2]
 
-nestedPi :: [Name] -> Expr -> Doc
-nestedPi l t@(Pi _ x t1 t2) | not (isFree 0 t2) = text ", " <> tprint 0 l t
-	                    | isPi t2 = text " " <> printTyDef l x t1 <> nestedPi (x:l) t2
-			    | otherwise = printTyDef l x t1 <> text "," <+> tprint 0 (x:l) t2
+nestedLamDef :: Expr -> ([(Name,Expr)], Expr)
+nestedLamDef (Lam _ x t1 t2) = ((x,t1):ds, e)
+                               where (ds, e) = nestedLamDef t2
+nestedLamDef t = ([], t)
+
+nestedPiDef :: Expr -> ([(Name,Expr)], Expr)
+nestedPiDef t@(Pi _ x t1 t2) | not (isFree 0 t2) = ([], t) 
+                             | otherwise = ((x,t1):ds, e)
+                                           where (ds, e) = nestedPiDef t2
+nestedPiDef t = ([], t)
 
 nestedLam :: [Name] -> Expr -> Doc
-nestedLam l t@(Lam _ x ty u) | isLam u = text " " <> printTyDef l x ty <> nestedLam (x:l) u
-                             | otherwise = text " " <> printTyDef l x ty <> text " =>" <+> tprint 0 (x:l) u
+nestedLam l t = printTyDef l False es <> text " =>" <+> tprint 0 xs r
+                where (es, r) = nestedLamDef t
+                      xs = reverse (map fst es) ++ l
 
-printTyDef :: [Name] -> Name -> Expr -> Doc
-printTyDef l x t = parens $ text (x ++ " : ") <> tprint 0 l t
+nestedPi :: [Name] -> Expr -> Doc
+nestedPi l t = printTyDef l False es <> text "," <+> tprint 0 xs r
+               where (es, r) = nestedPiDef t
+                     xs = reverse (map fst es) ++ l
+
+printTyDef :: [Name] -> Bool -> [(Name,Expr)] -> Doc
+printTyDef _ _ [] = empty
+printTyDef l p ((x,e):es) = parensIf (p || not (null es)) (text (x ++ " : ") <> tprint 0 l e)
+                            <+> printTyDef (x:l) True es
