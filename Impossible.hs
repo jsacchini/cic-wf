7d8ec2d7ce503be8cd4cd23609f1179000ccc4a3
Jorge Luis Sacchini <Jorge-Luis.Sacchini@inria.fr>
2010-02-05 16:24:14 +0000

    Added Impossible (stolen from Agda)

---
+++ Impossible.hs
@@ -0,0 +1,34 @@
+------------------------------------------------------------------------
+-- | An interface for reporting \"impossible\" errors
+------------------------------------------------------------------------
+
+{-# LANGUAGE DeriveDataTypeable #-}
+
+module Impossible where
+
+import Control.Exception as E
+import Data.Typeable
+
+-- | \"Impossible\" errors, annotated with a file name and a line
+-- number corresponding to the source code location of the error.
+
+data Impossible = Impossible String Integer deriving Typeable
+
+instance Show Impossible where
+  show (Impossible file line) = unlines
+    [ "An internal error has occurred. Please report this as a bug."
+    , "Location of the error: " ++ file ++ ":" ++ show line
+    ]
+
+instance Exception Impossible
+
+-- | Abort by throwing an \"impossible\" error. You should not use
+-- this function directly. Instead use the macro in @undefined.h@.
+
+throwImpossible :: Impossible -> a
+throwImpossible = throw
+
+-- | Catch an \"impossible\" error, if possible.
+
+catchImpossible :: IO a -> (Impossible -> IO a) -> IO a
+catchImpossible = E.catch
