22970797c1eb7273bf9d929314f91b58c00a5afa
Jorge Luis Sacchini <Jorge-Luis.Sacchini@inria.fr>
2010-03-09 19:08:59 +0000

    Utils.Misc: minor change

--- Utils/Misc.hs
+++ Utils/Misc.hs
@@ -8,6 +8,9 @@ appFst f (x,y) = (f x, y)
 appSnd :: (a -> b) -> (c, a) -> (c, b)
 appSnd f (x,y) = (x, f y)
 
+appPair :: (a -> b) -> (a, a) -> (b, b)
+appPair f (x,y) = (f x, f y)
+
 mWhen :: (Monad m) => m Bool -> m () -> m ()
 mWhen c n = do b <- c
                when b n
