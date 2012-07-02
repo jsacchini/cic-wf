3e3853a0f1aaf548aed456eba7d19157b42701f9
Jorge Luis Sacchini <sacchini@qatar.cmu.edu>
2012-07-02 20:39:46 +0100

    Added copyright information. Changes cabal package name to cicminus.

--- Syntax/Position.hs
+++ Syntax/Position.hs
@@ -1,3 +1,22 @@
+{- cicminus
+ - Copyright 2011 by Jorge Luis Sacchini
+ -
+ - This file is part of cicminus.
+ -
+ - cicminus is free software: you can redistribute it and/or modify it under the
+ - terms of the GNU General Public License as published by the Free Software
+ - Foundation, either version 3 of the License, or (at your option) any later
+ - version.
+
+ - cicminus is distributed in the hope that it will be useful, but WITHOUT ANY
+ - WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ - FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ - details.
+ -
+ - You should have received a copy of the GNU General Public License along with
+ - cicminus. If not, see <http://www.gnu.org/licenses/>.
+ -}
+
 -- | Positions and ranges are used to print information about where errors occur
 
 module Syntax.Position where
